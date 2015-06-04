12ed5c89419f8a563a4a7020a7d930853c59d5e3
Yannick Marcon <yannick.marcon@obiba.org>
2015-06-03 21:45:48 -0400

    code indentation
    Former-commit-id: bffb2ac978c9ccc83fb41e62f87d7a5dc084ed56

--- bin/start.sh
+++ bin/start.sh
@@ -15,8 +15,8 @@ cd /app && \
   drush en -y obiba_mica && \
   drush dl -y jquery_update && \
   drush en -y jquery_update && \
-	drush vset -y jquery_update_jquery_version 1.8 && \
-	drush vset -y jquery_update_jquery_admin_version 1.8 && \
+  drush vset -y jquery_update_jquery_version 1.8 && \
+  drush vset -y jquery_update_jquery_admin_version 1.8 && \
   drush vset -y mica_url https://$MICA_PORT_8445_TCP_ADDR:8445 && \
   drush vset -y jquery_update_jquery_version 1.8 && \
   drush vset -y jquery_update_jquery_admin_version 1.8 && \
@@ -26,8 +26,8 @@ cd /app && \
   drush vset -y obiba-progressbar-file "dist/obiba-progressbar" && \
   drush obiba-progressbar-download 1.0.0 && \
   drush en -y obiba_main_app_angular && \
-	rm -rf sites/all/libraries/angular-app  && \
-	drush angular-app && \
+  rm -rf sites/all/libraries/angular-app  && \
+  drush angular-app && \
   drush en -y obiba_auth && \
   drush vset -y mica_url https://$AGATE_PORT_8444_TCP_ADDR:8444 && \
   drush en -y obiba_mica_data_access_request
