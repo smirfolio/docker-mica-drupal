87445474ea463989fafd4e7ad61e7656e6c26f91
Yannick Marcon <yannick.marcon@obiba.org>
2015-06-03 21:16:25 -0400

    agate and angular update
    Former-commit-id: e306882c826e1374dda168a85c04a07b84a0c54b

--- bin/start.sh
+++ bin/start.sh
@@ -13,9 +13,10 @@ cd /app && \
   drush vset -y theme_default obiba_bootstrap && \
   drush vset -y admin_theme seven && \
   drush en -y obiba_mica && \
-  drush en -y obiba_auth && \
   drush dl -y jquery_update && \
   drush en -y jquery_update && \
+	drush vset -y jquery_update_jquery_version 1.8 && \
+	drush vset -y jquery_update_jquery_admin_version 1.8 && \
   drush vset -y mica_url https://$MICA_PORT_8445_TCP_ADDR:8445 && \
   drush vset -y jquery_update_jquery_version 1.8 && \
   drush vset -y jquery_update_jquery_admin_version 1.8 && \
@@ -23,6 +24,12 @@ cd /app && \
   drush datatables-plugins-download && \
   drush vset -y obiba-progressbar-lib obiba-progressbar-1.0.0 && \
   drush vset -y obiba-progressbar-file "dist/obiba-progressbar" && \
-  drush obiba-progressbar-download 1.0.0
+  drush obiba-progressbar-download 1.0.0 && \
+  drush en -y obiba_main_app_angular && \
+	rm -rf sites/all/libraries/angular-app  && \
+	drush angular-app && \
+  drush en -y obiba_auth && \
+  drush vset -y mica_url https://$AGATE_PORT_8444_TCP_ADDR:8444 && \
+  drush en -y obiba_mica_data_access_request
 
 supervisord -n
