adcfcbe717975966d4e2d52bc67b94f54c50e1da
Yannick Marcon <yannick.marcon@obiba.org>
2015-11-16 14:11:03 -0500

    chmod on drupal sites
    Former-commit-id: 6a41bc9829eab3b116194a34e8404edbf6f5b3a6

--- bin/start.sh
+++ bin/start.sh
@@ -7,6 +7,7 @@ cd /tmp/mica2-home-master && \
   make enable-modules setup-dependencies drupal_dir=/var/www/html && \
   cd /var/www/html && \
   drush vset -y mica_url https://$MICA_PORT_8445_TCP_ADDR:8445 && \
-  drush vset -y agate_url https://$AGATE_PORT_8444_TCP_ADDR:8444
+  drush vset -y agate_url https://$AGATE_PORT_8444_TCP_ADDR:8444 && \
+  chown -R www-data:www-data sites
 
 apache2-foreground
