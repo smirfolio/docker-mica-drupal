41513566728f6675bc40751b7fcb178b3984b1d0
samirMtl <ben.sghaier.samir@gmail.com>
2016-01-15 18:14:15 -0500

    Script update to deploy on nightly
    Former-commit-id: 74e8cb55b2871ff16096ee7b159d3beef4d88a4f

--- bin/first_run.sh
+++ bin/first_run.sh
@@ -19,4 +19,13 @@ cd /var/www/html && \
   drush vset -y agate_url https://$AGATE_PORT_8444_TCP_ADDR:8444 && \
   drush upwd administrator --password=$DRUPAL_ADMINISTRATOR_PASSWORD && \
   drush vset -y mica_anonymous_password $MICA_ANONYMOUS_PASSWORD && \
+  drush en -y obiba_mica_graphic && \
+  php -r "print json_encode(array('NaN'=>'NaN'));"  | drush vset --format=json graphics_blocks_networks - &&\
+  php -r "print json_encode(array('populations-selectionCriteria-countriesIso'=>'populations-selectionCriteria-countriesIso', 'populations-recruitment-dataSources'=>'populations-recruitment-dataSources', 'methods-designs'=>'methods-designs', 'populations-dataCollectionEvents-bioSamples'=>'populations-dataCollectionEvents-bioSamples','access'=>'access'));"  | drush vset --format=json graphics_blocks_aggregations - && \
+  drush vset -y countriesIso_css  col-md-6
+  drush vset -y recruitment-dataSources_css  col-md-6
+  drush vset -y methods-designs_css  col-md-6
+  drush vset -y populationDceBioSamples_css  col-md-6
+  drush vset -y access_css  col-md-6
+  echo "\$base_url = 'https://nightly.obiba.org';" >> sites/default/settings.php && \
   chown -R www-data:www-data .
